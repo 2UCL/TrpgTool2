4bcde97f94fb19d0662ee07e9424885e67f9f56a
2UCL <seconduser18.2@outlook.com>
2025-01-13 03:04:40 +0900

    v0.7.0 update
    UPDATELOG.md

--- js/vampireV.js
+++ js/vampireV.js
@@ -1,88 +1,206 @@
 let randomCount = 0;
 
+const export_options = {
+    "sort_order" : 0,
+    "changed_only" : true,
+    "dice_prefix" : "CCB"
+};
+
 const btnRND = document.createElement("input");
     btnRND.id="xBtnRnd";
     btnRND.onclick=random_sure;
-    btnRND.value="RANDOMIZE";
+    btnRND.value="ALL RANDOMIZE";
     btnRND.type="button";
     btnRND.classList.add("xbtn","btn-xrnd");
 
-const selects = [
-    document.getElementById("NA1"),
-    document.getElementById("NA2"),
-    document.getElementById("NA3"),
-    document.getElementById("NA4"),
-    document.getElementById("NA5"),
-    document.getElementById("NA6"),
-    document.getElementById("NA7"),
-    document.getElementById("NA8")
-]
+const selects = Array.from({ length: 8 }, (_, i) => document.getElementById(`NA${i + 1}`));
 
 function export_CCF(){
     var req = new XMLHttpRequest();
     req.onreadystatechange = function() {
         // respnse OK & connection OK then...
-        if(req.readyState == 4){
-            if (req.status == 200){
-                var base ={} , obj = {}, stat = [{},{},{}], pram = [{},{},{},{},{},{},{},{}], cmd = "";
-                const source = JSON.parse(req.responseText);
+        if( req.readyState == 4 ){
+            if ( req.status == 200 ){
+                var base = {}, 
+                    obj = {}, 
+                    stat = Array(3).fill({}).map(() => ({})),
+                    pram = Array(8).fill({}).map(() => ({})),
+                    cmd = "";
 
-                // base info
-                obj.name = source.pc_name;
-                obj.memo = "via TrpgTool2";
-                obj.initiative = Number(source.NP4);
-                obj.externalUrl = location.href;
+                try {    
+                    const source = JSON.parse(req.responseText);
+    
+                    // base info
+                    obj.name = source.pc_name;
+                    obj.memo = "via TrpgTool2";
+                    obj.initiative = Number(source.NP4);
+                    obj.externalUrl = location.href;
+    
+                    // status
+                    stat[0].label = "HP";
+                    stat[0].value = source.NP9;
+                    stat[0].max = source.NP9;
+                    stat[1].label = "MP";
+                    stat[1].value = source.NP10;
+                    stat[1].max = source.NP10;
+                    stat[2].label = "SAN";
+                    stat[2].value = source.SAN_Left;
+                    stat[2].max = source.SAN_Left;
+                    obj.status = stat;
+                    
+                    // params
+                    const labels = ["STR","CON","POW","DEX","APP","SIZ","INT","EDU"];
+                    pram.forEach((item, index) => {
+                        item.label = labels[index];
+                        item.value = source[`NP${index + 1}`];
+                    });
+                    // damage bonus
+                    pram.push({"label":"db", "value":source.dmg_bonus});
+                    obj.params = pram;
+    
+                    // commands - chat pallet
+                    
+                    // main skill
+                    // T[B]A(P) -> 
+                    // [SKILL] Battle, F?Search, Act, Communicate?, Knowledge
+                    // ( TYPE) <U>, Default, Shokugyo, Kyomi, A?(Seicho), Other, P?(Total)
+                    const skill = ["TBA","TFA","TAA","TCA","TKA"];
+                    let skill_name = [
+                        ["回避","キック","組み付き","こぶし（パンチ）","頭突き","投擲","マーシャルアーツ","拳銃","サブマシンガン","ショットガン","マシンガン","ライフル"],
+                        ["応急手当","鍵開け","隠す","隠れる","聞き耳","忍び歩き","写真術","精神分析","追跡","登攀","図書館","目星"],
+                        ["運転","機械修理","重機械操作","乗馬","水泳","製作","操縦","跳躍","電気修理","ナビゲート","変装"],
+                        ["言いくるめ","信用","説得","値切り","母国語"],
+                        ["医学","オカルト","化学","クトゥルフ神話","芸術","経理","考古学","コンピューター","心理学","人類学","生物学","地質学","電子工学","天文学","博物学","物理学","法律","薬学","歴史"]
+                    ];
+                    // option name
+                    if (source.unten_bunya != "") skill_name[2][0] += `(${source.unten_bunya})`;
+                    if (source.seisaku_bunya != "") skill_name[2][5] += `(${source.seisaku_bunya})`;
+                    if (source.main_souju_norimono != "") skill_name[2][6] += `(${source.main_souju_norimono})`;
+                    if (source.mylang_name != "") skill_name[3][4] += `(${source.mylang_name})`;
+                    if (source.geijutu_bunya != "") skill_name[4][4] += `(${source.geijutu_bunya})`;
+                    // external skill name
+                    skill.forEach((item, index) => {
+                        try {
+                            skill_name[index] = skill_name[index].concat(source[`${item}Name`]);
+                        } catch (error) {
+                            console.log("External skill name:", error);
+                        }
+                    });
+    
+                    // header command
+                    cmd  = `${export_options.dice_prefix}<={SAN} 【SAN値チェック】\n`;
+                    cmd += `${export_options.dice_prefix}<=${source.NP12} 【アイデア】\n`;
+                    cmd += `${export_options.dice_prefix}<=${source.NP13} 【幸運】\n`;
+                    cmd += `${export_options.dice_prefix}<=${source.NP14} 【知識】\n`;
+                    cmd += "　\n== 能力値 * 5 ====\n";
+                    labels.forEach( element => {
+                        cmd += `${export_options.dice_prefix}<=({${element}}*5) 【${element}】\n`
+                    });
+                    cmd += "　\n== 技能値 ====\n";
+    
+                    // base data
+                    var accessKey;
+                    let queue = [];
+                    skill.forEach( ( key, k_index ) => {
+                        accessKey = `${key}P`;
+                        source[accessKey].forEach( ( item, index ) => {
+                            var line = {};
+                            
+                            line.value = Number(item);
+                            line.label = skill_name[k_index][index];
+                            
+                            if ( ( export_options.changed_only && source[`${key}D`][index] != item ) || !export_options.changed_only ) queue.push(line);
+                        });
+                    });
+    
+                    // data sort
+                    var last_value = null;
+                    switch ( export_options.sort_order ){
+                        // 標準
+                        case 0:
+                            queue.forEach( element => {
+                                cmd += `${export_options.dice_prefix}<=${element.value} 【${element.label}】\n`
+                            });
+                            break;
+    
+                        // 降順
+                        case 1:
+                            queue.sort( (a, b) => {
+                                return (a.value > b.value) ? -1 : 1;
+                            });
+                            queue.forEach( element => {
+                                cmd += `${export_options.dice_prefix}<=${element.value} 【${element.label}】\n`
+                            });
+                            break;
+    
+                        // 降順 まとめる
+                        case 2:
+                            queue.sort( (a, b) => {
+                                return (a.value > b.value) ? -1 : 1;
+                            });
+                            queue.forEach( element => {
+                                if ( last_value != element.value ) {
+                                    if ( last_value != null ) cmd += "】\n";
+                                    cmd += `${export_options.dice_prefix}<=${element.value} 【${element.label}`;
+                                    last_value = element.value;
+                                } else {
+                                    cmd += `, ${element.label}`;
+                                }
+                            });
+                            cmd += "】\n";
+                            break;
+    
+                        // 昇順
+                        case 3:
+                            queue.sort( (a, b) => {
+                                return (a.value < b.value) ? -1 : 1;
+                            });
+                            queue.forEach( element => {
+                                cmd += `${export_options.dice_prefix}<=${element.value} 【${element.label}】\n`
+                            });
+                            break;
+    
+                        // 昇順 まとめる
+                        case 4:
+                            queue.sort( (a, b) => {
+                                return (a.value < b.value) ? -1 : 1;
+                            });
+                            queue.forEach( element => {
+                                if ( last_value != element.value ) {
+                                    if ( last_value != null ) cmd += "】\n";
+                                    cmd += `${export_options.dice_prefix}<=${element.value} 【${element.label}`;
+                                    last_value = element.value;
+                                } else {
+                                    cmd += `, ${element.label}`;
+                                }
+                            });
+                            cmd += "】\n";
+                            break;
+                    };
+    
+                    obj.commands = cmd;
+                    
+    
+                    base.kind = "character";
+                    base.data = obj;
+    
+                    const expo = JSON.stringify(base, null,"");
+                    navigator.clipboard.writeText(expo);
+    
+                    info("クリップボードへの出力が完了しました。CCFOLIAの部屋上で 貼り付け(Ctrl+V) が可能です。");
+                    scroll_top();
 
-                // status
-                stat[0].label = "HP";
-                stat[0].value = source.NP9;
-                stat[0].max = source.NP9;
-                stat[1].label = "MP";
-                stat[1].value = source.NP10;
-                stat[1].max = source.NP10;
-                stat[2].label = "SAN";
-                stat[2].value = source.SAN_Left;
-                stat[2].max = source.SAN_Left;
-                obj.status = stat;
-                
-                // params
-                pram[0].label = "STR";
-                pram[0].value = source.NP1;
-                pram[1].label = "CON";
-                pram[1].value = source.NP2;
-                pram[2].label = "POW";
-                pram[2].value = source.NP3;
-                pram[3].label = "DEX";
-                pram[3].value = source.NP4;
-                pram[4].label = "APP";
-                pram[4].value = source.NP5;
-                pram[5].label = "SIZ";
-                pram[5].value = source.NP6;
-                pram[6].label = "INT";
-                pram[6].value = source.NP7;
-                pram[7].label = "EDU";
-                pram[7].value = source.NP8;
-                obj.params = pram;
+                } catch (error) {
+                    console.log("JSON parse:", error);        
+                }
 
-                // commands - chat pallet
-                
-                obj.commands = cmd;
-                
-
-                base.kind = "character";
-                base.data = obj;
-
-                const expo = JSON.stringify(base, null,"");
-                navigator.clipboard.writeText(expo);
-
-                info("クリップボードへの出力が完了しました。CCFOLIAの部屋上で 貼り付け(Ctrl+V) が可能です。");
-                scroll_top();
             }else{
                 alert("データ取得に失敗しました ("+req.status+")");
             }
+
         }
     };
-    req.open("GET", location.href+".js", false);
+    req.open("GET", location.href + ".js", true);
     req.send(null);
     
 }
@@ -99,6 +217,7 @@ function export_JSO(){
 }
 
 function export_CPE(){
+    var palette = ""
 
 }
 
@@ -249,9 +368,12 @@ function vMain(){
     // add object
     const side = document.querySelector("aside.leftsidebar.fixed");
 
+    // temporary fix
+    document.getElementsByClassName("trifull btn btn-info")[0].innerHTML = "<span class=\"fa fa-file-text-o\"></span> TXT"
+
     const base = document.createElement("section");
         const h4 = document.createElement("h4");
-            const title = document.createTextNode("TrpgTool2 Extentions");
+            const title = document.createTextNode("TrpgTool2 Extention");
             h4.appendChild(title);
         base.appendChild(h4);
     
@@ -270,10 +392,13 @@ function vMain(){
                         opt = document.createElement("option");
                         opt.setAttribute("value", optCHP[i]);
                         opt.innerHTML = optCHP[i];
-                        if (i == 0) opt.setAttribute("selected", true);
+                        if (i == export_options.sort_order) opt.setAttribute("selected", true);
                         
                         selCHP.appendChild(opt);
                     }
+                    selCHP.onchange = function(){
+                        export_options.sort_order = this.selectedIndex;
+                    };
                     
                     lblCHP.appendChild(selCHP);
                 spnOPT.appendChild(lblCHP);
@@ -283,6 +408,9 @@ function vMain(){
                     const chkACT = document.createElement("input");
                     chkACT.type="checkbox";
                     chkACT.checked = true;
+                    chkACT.onchange = function(){
+                        export_options.changed_only = this.checked;
+                    }
                     lblACT.appendChild(chkACT);
                     lblACT.appendChild(document.createTextNode(" 変更された技能のみ出力"));
                 spnOPT.appendChild(lblACT);
@@ -302,6 +430,10 @@ function vMain(){
                         opt.setAttribute("value", "CC");
                         opt.innerHTML = "CC";
                         selPRF.appendChild(opt);
+
+                        selPRF.onchange = function(){
+                            export_options.dice_prefix = this.value;
+                        };
 
                     lblPRF.appendChild(selPRF);
                 spnOPT.appendChild(lblPRF);
@@ -338,7 +470,7 @@ function vMain(){
                 btnCPE.classList.add("full","xbtn","btn-xccf");
                 
                 spnCPE.appendChild(btnCPE);
-            main.appendChild(spnCPE);
+            // main.appendChild(spnCPE);
 
             // json export
             const spnJSO = document.createElement("span");
